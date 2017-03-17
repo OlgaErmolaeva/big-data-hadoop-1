605379d146ed7bb77d83c84c7be6567d9166137f
Olga Ermolaeva <olga_ermolaeva@epam.com>
2017-03-17 21:28:18 +0100

    Changed main class. Now extends Configured implements Tool

--- src/main/java/hadoop_1/LongestWordCounter.java
+++ src/main/java/hadoop_1/LongestWordCounter.java
@@ -1,21 +1,31 @@
 package hadoop_1;
 
 
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.conf.Configured;
+import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.NullWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
-import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.util.Tool;
+import org.apache.hadoop.util.ToolRunner;
+import org.apache.log4j.Logger;
 
-import java.io.IOException;
+
+public class LongestWordCounter extends Configured implements Tool {
 
 
-public class LongestWordCounter {
+    public static void main(String[] args) throws Exception {
+        LongestWordCounter longestWordCounter = new LongestWordCounter();
+        ToolRunner.run(new Configuration(), longestWordCounter, args);
+    }
 
-    public static void main(String[] args) throws IOException {
-        Job job = Job.getInstance();
+    public int run(String[] args) throws Exception {
+        Configuration conf = this.getConf();
+        Job job = Job.getInstance(conf);
         job.setJarByClass(LongestWordCounter.class);
         job.setMapperClass(LongestWordMapper.class);
         job.setReducerClass(LongestWordReducer.class);
@@ -24,16 +34,8 @@ public class LongestWordCounter {
         job.setOutputKeyClass(IntWritable.class);
         job.setOutputValueClass(Text.class);
 
-
-        FileInputFormat.addInputPath(job, new Path("/input.txt"));
-        FileOutputFormat.setOutputPath(job, new Path("output.txt"));
-
-        try {
-            System.exit(job.waitForCompletion(true)? 0:1);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        } catch (ClassNotFoundException e) {
-            e.printStackTrace();
-        }
+        FileInputFormat.addInputPath(job, new Path(args[0]));
+        FileOutputFormat.setOutputPath(job, new Path(args[1]));
+        return (job.waitForCompletion(true) ? 0 : 1);
     }
 }
